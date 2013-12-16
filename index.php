ab6a77e3a8417c35261f53435c4059b87c1be1f9
jgrietveld <jerry@jgrietveld.com>
2013-12-16 16:45:54 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Convert your content to HTML</title>
+<!-- Place inside the <head> of your HTML -->
+<script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+tinymce.init({
+    selector: "textarea"
+ });
+</script>
+</head>
+<body>
+
+
+<!-- Place this in the body of the page content -->
+<form method="post">
+    <textarea></textarea>
+</form>
+
+
+</body>
+</html>
