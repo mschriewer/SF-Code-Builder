d52a94be87c6f5c015a3a1f9da9f40d0cd24de99
jgrietveld <jerry@jgrietveld.com>
2013-12-16 17:36:59 +0000

    Adding code view

--- index.php
+++ index.php
@@ -6,29 +6,20 @@
 <script type="text/javascript">
 tinymce.init({
     selector: "textarea",
-    theme: "modern",
     plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor moxiemanager"
-    ],
-    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    toolbar2: "print preview media | forecolor backcolor emoticons",
-    image_advtab: true,
-    templates: [
-        {title: 'Test template 1', content: 'Test 1'},
-        {title: 'Test template 2', content: 'Test 2'}
+        "code fullscreen"
     ]
-});
+
+ });
 </script>
+
 </head>
 <body>
 
 
 <!-- Place this in the body of the page content -->
-<form method="post" action="somepage">
-    <textarea name="content" style="width:100%"></textarea>
+<form method="post">
+    <textarea></textarea>
 </form>
 
 
