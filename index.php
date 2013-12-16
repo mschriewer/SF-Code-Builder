908fca2efd9b8ad2661133a245a0b54d327d6ac8
jgrietveld <jerry@jgrietveld.com>
2013-12-16 17:26:55 +0000

    Adding code view

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 	<title>Convert your content to HTML</title>
 <!-- Place inside the <head> of your HTML -->
-<script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
+<script type="text/javascript" src="<your installation path>/tinymce/tinymce.min.js"></script>
 <script type="text/javascript">
 tinymce.init({
     selector: "textarea",
@@ -15,16 +15,20 @@ tinymce.init({
     ],
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     toolbar2: "print preview media | forecolor backcolor emoticons",
-    image_advtab: true
- });
+    image_advtab: true,
+    templates: [
+        {title: 'Test template 1', content: 'Test 1'},
+        {title: 'Test template 2', content: 'Test 2'}
+    ]
+});
 </script>
 </head>
 <body>
 
 
 <!-- Place this in the body of the page content -->
-<form method="post">
-    <textarea></textarea>
+<form method="post" action="somepage">
+    <textarea name="content" style="width:100%"></textarea>
 </form>
 
 
