b316b7e13f40b0bafbd075438fc04688e6394629
jgrietveld <jerry@jgrietveld.com>
2013-12-16 17:13:16 +0000

    Adding code view

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
 <script type="text/javascript">
 tinymce.init({
-    selector: "textarea"
-
+    selector: "textarea",
+    theme: "modern",
     plugins: [
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
