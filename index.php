1e5a6068893fd3373e1ae04692cef23ba8264550
jgrietveld <jerry@jgrietveld.com>
2013-12-16 17:11:37 +0000

    Adding code view

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 <script type="text/javascript">
 tinymce.init({
     selector: "textarea"
+
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor moxiemanager"
+    ],
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true
  });
 </script>
 </head>
