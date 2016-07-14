a73991b0daecb8144a6c120d137c606fedb5cdb2
Mike Schriewer <mschriewer@mschriewer-ltm2.internal.salesforce.com>
2016-07-14 18:28:25 +0200

    index.php changed

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ tinymce.init({
  });
 
 </script>
-<!--script src="js/jQuery/jquery-1.6.1.min.js"></script-->
+<script src="js/jQuery/jquery-1.6.1.min.js"></script>
 
 <!--script src="http:////ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script-->
 <script src="js/code.js"></script>
